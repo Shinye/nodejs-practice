89e4aba45afb6348ba124af2d3d9f1d2c44749f4
Shinye Song <novembertwentysix@gmail.com>
2017-05-01 19:56:08 +0900

    nodejs실습 : ajax / view engine

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser')
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
 
 app.listen(3000, function(){
     console.log("starting express server on port 3000!!!!");
 }); // 3000번 포트를 접속하는거
 
-app.use(express.static('public')) // use : 야 나 이거 쓸랭!!
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static('public')); // use : 야 나 이거 쓸랭!!
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs') // 나 view engine은 ejs쓸래!!!
 
-// public폴더에 들어있는 모든 파일들을 static파일들로 간주해 아래와 같이 url 라우팅 
+// public폴더에 들어있는 모든 파일들을 static파일들로 간주해 아래와 같이 url 라우팅
 // 을 처리하지 않아도 파일을 불러오는데 문제가 없도록 해주는 함수.
 
 // url routing!!
@@ -23,7 +24,13 @@ app.get('/',function(req,res){
 app.post('/email_post', function(req,res){
     // get : req.param('email')
     console.log(req.body.email); // email: 'nicole9111@naver.com' 의 형식으로 나오기때문에
-    res.send("<h1>hello "+req.body.email+"</h1>");
-    
+    //res.send("<h1>hello "+req.body.email+"</h1>");
+    res.render('email.ejs', {'email' : req.body.email})
 });
 
+app.post('/ajax_send_email', function(req,res){
+  //console.log(req.body.email);
+  var responseData = {'result':'ok', 'email':req.body.email};
+  res.json(responseData);
+
+});
