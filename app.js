71c0b0072b3bd2f058e146b627719a42f5cd8387
Shinye Song <novembertwentysix@gmail.com>
2017-05-03 21:01:20 +0900

    코드 해석 및 정리

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser'); // 바디에 적힌 데이터들을 파싱해줌
 
 
 app.listen(3000, function(){
@@ -8,7 +8,7 @@ app.listen(3000, function(){
 }); // 3000번 포트를 접속하는거
 
 app.use(express.static('public')); // use : 야 나 이거 쓸랭!!
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // 바디 안의 json 형태의 데이터를 json으로 파싱한다?
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs') // 나 view engine은 ejs쓸래!!!
 
@@ -21,7 +21,7 @@ app.get('/',function(req,res){
     res.sendFile(__dirname + "/public/main.html");
 });
 
-app.post('/email_post', function(req,res){
+app.post('/email_post', function(req,res){ // HTTP POST요청으로 이 URL의(첫번째 인자) 내용을 가져오도록 만든다.
     // get : req.param('email')
     console.log(req.body.email); // email: 'nicole9111@naver.com' 의 형식으로 나오기때문에
     //res.send("<h1>hello "+req.body.email+"</h1>");
@@ -29,8 +29,6 @@ app.post('/email_post', function(req,res){
 });
 
 app.post('/ajax_send_email', function(req,res){
-  //console.log(req.body.email);
   var responseData = {'result':'ok', 'email':req.body.email};
   res.json(responseData);
-
 });
